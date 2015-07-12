8289ec1a88d3cc3f8aac36d3ffdc2b91ba1e464f
Jessica Lee <jessicalee@Jessicas-MacBook-Air.local>
2015-07-11 19:11:51 -0700

    update

--- MainPanel.java
+++ MainPanel.java
@@ -1,5 +1,28 @@
-
-public class MainPanel extends JPanel implements 
+public class MainPanel extends JPanel implements ActionListener
 {
+    TitlePanel() titleP = new TitlePanel();
+    private JTextArea ageInput; 
+    public MainPanel()
+    {
+        setLayout(null);
+        setBackground(Color.PINK);
+        titleP.setBounds(1,1,1,1);
+        ageInput = new JTextArea
+        
+        add(titleP);
+        
 
+    }
+    class TitlePanel extends JPanel
+    {
+        public TitlePanel()
+        {
+            setPreferredSize(new Dimension(40,100));
+        }
+        
+        public void paintComponent(Graphics g)
+        {
+               super.paintComponent(g);
+        }
+    }
 }
